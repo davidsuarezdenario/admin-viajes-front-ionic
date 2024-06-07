8983d8042be7cd8a709add91c7e2a35d34661036
yarednicolas23 <yarednicolas@gmail.com>
2024-06-06 21:14:51 -0500

    add: markup module route and form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ export const routes: Routes = [
   { path: 'dashboard', loadComponent: () => import('./pages/dashboard/dashboard.page').then( m => m.DashboardPage) },
   { path: 'home', loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage), },
   { path: 'login', loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage) },
+  { path: 'markup', loadComponent: () => import('./pages/markup/markup.page').then( m => m.MarkupPage) },
   { path: 'operaciones', loadComponent: () => import('./pages/operaciones/operaciones.page').then( m => m.OperacionesPage) },
   { path: 'convenios', loadComponent: () => import('./pages/convenios/convenios.page').then( m => m.ConveniosPage) },
   { path: 'usuarios', loadComponent: () => import('./pages/usuarios/usuarios.page').then( m => m.UsuariosPage) },
---
+++ src/app/pages/markup/markup.page.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonLabel, IonItem, IonSelectOption } from '@ionic/angular/standalone';
+import { HeaderMainComponent } from "../../shared/components/header-main/header-main.component";
+import { GlobalService } from "../../shared/services/global/global.service";
+
+interface MarkupConfiguration {
+  name: string;
+  fields: string[]; // Lista de campos configurables
+}
+
+@Component({
+  selector: 'app-markup',
+  templateUrl: './markup.page.html',
+  styleUrls: ['./markup.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonLabel, IonItem, IonSelectOption, CommonModule, FormsModule, HeaderMainComponent ]
+})
+export class MarkupPage implements OnInit {
+
+  selectedMarkupType: string = ''; // Asigna un valor inicial aquí
+  configurableFields: string[] = [];
+  formData: { [field: string]: any } = {};
+
+  private configurations: { [markupType: string]: MarkupConfiguration } = {
+    'vuelos': {
+      name: 'Vuelos',
+      fields: ['Aerolínea', 'País Destino', 'Ciudad Destino', 'País Origen', 'Ciudad Origen', 'Fecha Ida', 'Fecha Vuelta', 'Rango Fechas', 'Tarifa']
+    },
+    'hoteles': {
+      name: 'Hoteles',
+      fields: ['País', 'Ciudad', 'Categoría', 'Rango Fechas']
+    }
+  };
+  constructor(
+    public globalService: GlobalService
+  ) { }
+
+  updateFields() {
+    this.configurableFields = this.configurations[this.selectedMarkupType].fields;
+    // Limpiar el formData
+    this.formData = {};
+  }
+
+  ngOnInit() {}
+
+  ionViewDidEnter() {
+    this.globalService.titleMain = 'Markup';
+  }
+
+  saveConfiguration() {
+    // Aquí puedes hacer lo que quieras con los datos del formulario
+    console.log('Datos del formulario:', this.formData);
+  }
+
+}
